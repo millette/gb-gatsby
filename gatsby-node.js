3b77385a0f64418a9e4a60d955a40ab658574fc0
Robin Millette <robin@millette.info>
2019-01-09 12:15:39 -0500

    Generate html pages from markdown.

---
+++ gatsby-node.js
@@ -0,0 +1,59 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// core
+const path = require("path")
+
+// npm
+const { createFilePath } = require("gatsby-source-filesystem")
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  // console.log('node.internal.type:', node.internal.type)
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = createFilePath({ node, getNode, basePath: "pages" })
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  // **Note:** The graphql function call returns a Promise
+  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+  const { createPage } = actions
+  return graphql(`
+    query {
+      allMarkdownRemark {
+        totalCount
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then((result) => {
+    // console.log(JSON.stringify(result, null, 4))
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      // console.log('NODE:', Object.keys(node))
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve("./src/templates/blog-post.js"),
+        context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          slug: node.fields.slug,
+        },
+      })
+    })
+  })
+}
