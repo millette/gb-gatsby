6a6db295b88959ea5266c81c90f2cbe3479677ac
Robin Millette <robin@millette.info>
2019-01-09 15:59:10 -0500

    Remove starter pages and images.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Default Starter",
+    title: "EDM5240-H2019",
     description:
-      "Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.",
-    author: "@gatsbyjs",
+      "Technologies de l'information appliquées au journalisme [ou journalisme informatique] - Session d'hiver 2019.",
+    author: "@jhroy",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: `${__dirname}/src/images`,
-      },
-    },
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -86,13 +79,13 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
+        name: "Technologies de l'information appliquées au journalisme",
+        short_name: "EDM5240-H2019",
         start_url: "/",
         background_color: "#663399",
         theme_color: "#663399",
         display: "minimal-ui",
-        icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
+        // icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
