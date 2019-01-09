01aa7336330e0720f0fcb1609e422d4908f0d7d7
Robin Millette <robin@millette.info>
2019-01-09 13:35:22 -0500

    Add summary to all pages.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,55 +1,33 @@
 // npm
 import React from "react"
-import { graphql, Link } from "gatsby"
-// import rehypeReact from "rehype-react"
-// import visit from 'unist-util-visit'
+import { graphql } from "gatsby"
 
 // self
 import Layout from "../components/layout"
 
-/*
-const renderAst = new rehypeReact({
-  createElement: React.createElement,
-  // components: { "interactive-counter": Counter },
-}).Compiler
-
-const visitor = (node) => {
-  // console.log('VIS #0:', node)
-  if (node.properties && node.properties.href) {
-    node.properties.href.replace(/\.md$/, '')
-  }
-  // console.log('VIS #1:', node)
-}
-*/
-
-export default ({ data }) => {
-  const { html, htmlAst, ...rest } = data.markdownRemark
-
-  // <div dangerouslySetInnerHTML={{ __html: post.html }} />
-  // {renderAst(htmlAst)}
-  // const g = visit(htmlAst, { tagName: 'a' }, visitor)
-
-  return (
-    <Layout>
-      <Link to="SUMMARY/">Sommaire</Link>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
-      <pre>{JSON.stringify(rest, null, "  ")}</pre>
-      <pre>{JSON.stringify(htmlAst, null, "  ")}</pre>
-    </Layout>
-  )
-}
+export default ({ data }) => (
+  <Layout>
+    <div
+      style={{ float: "left" }}
+      dangerouslySetInnerHTML={{ __html: data.summary.html }}
+    />
+    <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+  </Layout>
+)
 
 export const query = graphql`
   query($slug: String!) {
+    summary: markdownRemark(fields: { slug: { eq: "/SUMMARY/" } }) {
+      html
+    }
+
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
-      htmlAst
       wordCount {
         paragraphs
         sentences
         words
       }
-      fileAbsolutePath
       excerpt
       timeToRead
     }
