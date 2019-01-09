a38a449a8df8cceb3014f61ec4506c4e7e18c707
Robin Millette <robin@millette.info>
2019-01-09 16:04:46 -0500

    Remove style.

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+// import "./layout.css"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -20,7 +20,7 @@ const Layout = ({ children }) => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
-          style={{
+          style666={{
             margin: `0 auto`,
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
@@ -28,11 +28,7 @@ const Layout = ({ children }) => (
           }}
         >
           {children}
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
+          <footer>© {new Date().getFullYear()}</footer>
         </div>
       </>
     )}
