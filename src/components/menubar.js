438d34758ad50f5d66d3ee34ebb27269f8c128dc
Robin Millette <robin@millette.info>
2019-01-10 08:03:20 -0500

    New menubar component (only theme switcher for now).

---
+++ src/components/menubar.js
@@ -0,0 +1,61 @@
+// npm
+import React, { Component } from "react"
+
+const StyleButton = ({ title, active, pick }) => (
+  <button
+    onClick={pick}
+    className={`button is-small${active ? " is-primary" : ""}`}
+  >
+    {title}
+  </button>
+)
+
+class Menubar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { hidden: false, styles: [], buttons: [] }
+    this.pick = this.pick.bind(this)
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      const styles = document.querySelectorAll(
+        'link[rel="stylesheet"],link[rel="alternate stylesheet"]'
+      )
+      const buttons = []
+      styles.forEach(({ title, disabled }) =>
+        buttons.push({ title, active: !disabled })
+      )
+      this.setState({ styles, buttons })
+    }, 0)
+  }
+
+  pick(ev) {
+    // prevents some flickering
+    const buttons = this.state.buttons
+    this.state.styles.forEach((el, i) => {
+      if (el.title === ev.target.innerText) {
+        buttons[i].active = true
+      } else {
+        el.disabled = true
+        buttons[i].active = false
+      }
+    })
+    this.setState({ buttons })
+    this.state.styles.forEach((el) => {
+      el.disabled = el.title !== ev.target.innerText
+    })
+  }
+
+  render() {
+    return (
+      <div className="buttons">
+        {this.state.buttons.map((s, i) => (
+          <StyleButton key={i} {...s} pick={this.pick} />
+        ))}
+      </div>
+    )
+  }
+}
+
+export default Menubar
