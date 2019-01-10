a7d2813ebac1988ee85f713f6cb3c71bc2362197
Robin Millette <robin@millette.info>
2019-01-10 12:51:50 -0500

    Handle new stylesheet (not part of switcher).

--- src/components/menubar.js
+++ src/components/menubar.js
@@ -31,8 +31,9 @@ class Menubar extends Component {
         "link[rel='stylesheet'],link[rel='alternate stylesheet']"
       )
       const buttons = []
-      styles.forEach(({ title, disabled }) =>
-        buttons.push({ title, active: !disabled })
+      styles.forEach(
+        ({ title, disabled }) =>
+          title && buttons.push({ title, active: !disabled })
       )
       this.setState({ styles, buttons })
     }, 0)
@@ -41,12 +42,21 @@ class Menubar extends Component {
   pick(ev) {
     // prevents some flickering
     const buttons = this.state.buttons
-    this.state.styles.forEach((el, i) => {
-      if (el.title !== ev.target.innerText) el.disabled = true
+    let i = 0
+    this.state.styles.forEach((el) => {
+      if (!el.title) return
+      if (el.title === ev.target.innerText) {
+        const ggg = document.querySelector("body")
+        ggg.className = `bulma-${el.title}`
+      } else {
+        el.disabled = true
+      }
       buttons[i].active = el.title === ev.target.innerText
+      ++i
     })
     this.setState({ buttons })
     this.state.styles.forEach((el) => {
+      if (!el.title) return
       el.disabled = el.title !== ev.target.innerText
     })
   }
