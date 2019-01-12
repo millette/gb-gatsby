9972ed84e8a7c033c3248c2cfdf8a12822e3cb05
Robin Millette <robin@millette.info>
2019-01-12 15:29:29 -0500

    Add support for gif images (by copying).

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,6 +64,13 @@ module.exports = {
             },
           },
           "gatsby-remark-smartypants",
+          {
+            resolve: "gatsby-remark-copy-linked-files",
+            options: {
+              destinationDir: "assets",
+              ignoreFileExtensions: ["png", "jpg", "jpeg"],
+            },
+          },
         ],
       },
     },
